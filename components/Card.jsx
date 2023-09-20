d953adcafa8e1eb3ea2eafff3fbc30f31b9cd982
paalaleks <paalaleks@hotmail.com>
2023-09-20 10:06:31 +0200

    commit_24

--- components/Card.jsx
+++ components/Card.jsx
@@ -8,14 +8,14 @@ export default function Card({ href, src, alt, name, description, tags }) {
         width={1200}
         height={1200}
         src={src}
-        className="group-hover:brightness-105 transition duration-300 ease-in-out"
+        z
+        className="group-hover:brightness-110 transition duration-300 ease-in-out"
         alt={alt}
       />
 
-      <div className="text-color2 px-4 pb-4 pt-2 mt-2 group-hover:shadow group-hover:bg-eerie_black-500 group-hover:bg-opacity-40 transition duration-300 ease-in-out">
-        <h2 className="text-color1 m-0 p-0">{name}</h2>
+      <div className="text-color2 px-4 pb-4 pt-2 mt-2 group-hover:bg-eerie_black-500 group-hover:bg-opacity-60 transition duration-300 ease-in-out">
+        <h2 className="text-color1 m-0 p-0">{name}</h2>z
         <span className="mr-1">{description}</span>
-
         {tags.map((tag) => {
           return (
             <span
